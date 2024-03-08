dc6e33035672cb6cbcbe75057d6f1928e49aebdf
JF Hector <jfhector@kooth.com>
2024-03-08 17:54:19 +0000

    use create sendErrorResponse util in getTodo

--- src/app/routes/getTodos.ts
+++ src/app/routes/getTodos.ts
@@ -1,21 +1,12 @@
-import { isRFC7807Error } from '../../utils/RFC7807Error';
 import { getTodosFromDb } from '../../infra/db/';
 import { RequestHandler } from 'express' ;
+import { sendErrorResponse } from '../utils/sendErrorResponse';
 
 export const getTodos: RequestHandler = async (_, res) => {
   try {
     const todos = await getTodosFromDb();
     res.set('Content-Type', 'application/vnd+jfhector.todos-app-todos+json').json(todos);
-  } catch (e: any) {
-    if (isRFC7807Error(e)) {
-      res.set('Content-Type', 'application/problem+json').json(e.toJson());
-    } else {
-      res.set('Content-Type', 'application/problem+json').json({
-        status,
-        type: e.type || 'Unknown',
-        title: 'Unexpected error occurred',
-        detail: e.detail || e.message,
-      })
-    }
+  } catch (err: any) {
+    sendErrorResponse(res, err);
   }  
 }
