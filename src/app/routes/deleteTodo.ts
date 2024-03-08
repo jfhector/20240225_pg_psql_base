fcd08e431966b60de1d827966d02ff04caa92e00
JF Hector <jfhector@kooth.com>
2024-03-08 21:24:37 +0000

    use handleError middleware in deleteTodos

--- src/app/routes/deleteTodo.ts
+++ src/app/routes/deleteTodo.ts
@@ -1,14 +1,13 @@
 import { deleteTodoFromDb } from '../../infra/db/';
 import { RequestHandler } from 'express' ;
-import { sendErrorResponse } from '../utils/sendErrorResponse';
 
-export const deleteTodo: RequestHandler = async (req, res) => {
+export const deleteTodo: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
   try {
     await deleteTodoFromDb(id);
     res.status(200).json({ message: 'Todo deleted successfully, if it existed' });
     
   } catch (err: any) {
-    sendErrorResponse(res, err);
+    next(err);
   }
 }
