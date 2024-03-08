06c65bad8b64e5b04c0a2c32e15e30834fbbdf3b
JF Hector <jfhector@kooth.com>
2024-03-08 21:24:02 +0000

    placeholder catch uncaughtException and unhandledRejection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import "./app/router";
+
+process.on("uncaughtException", (err:Error) => {
+    console.error(err);
+    // TODO OPTIONAL also use this for uncaughtException and unhandledRejection, see 2.4 Handle errors centrally, not within a middleware
+});
+  
+process.on("unhandledRejection", (reason) => {
+    console.error(reason);
+    // TODO OPTIONAL also use this for uncaughtException and unhandledRejection, see 2.4 Handle errors centrally, not within a middleware
+});
