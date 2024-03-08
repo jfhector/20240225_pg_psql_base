12e7049fc8bfa92a5f9f8f0ff0818f28616adef5
JF Hector <jfhector@kooth.com>
2024-03-08 21:24:31 +0000

    use handleError middleware in getTodos

--- src/app/routes/getTodos.ts
+++ src/app/routes/getTodos.ts
@@ -1,12 +1,11 @@
 import { getTodosFromDb } from '../../infra/db/';
 import { RequestHandler } from 'express' ;
-import { sendErrorResponse } from '../utils/sendErrorResponse';
 
-export const getTodos: RequestHandler = async (_, res) => {
+export const getTodos: RequestHandler = async (_, res, next) => {
   try {
     const todos = await getTodosFromDb();
     res.set('Content-Type', 'application/vnd+jfhector.todos-app-todos+json').json(todos);
   } catch (err: any) {
-    sendErrorResponse(res, err);
+    next(err);
   }  
 }
