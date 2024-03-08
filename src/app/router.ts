fa6fbc525e30caf03c5fd33de1737d816331b7ae
JF Hector <jfhector@kooth.com>
2024-03-08 21:24:12 +0000

    add handleError middleware

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getTodos, postTodo, deleteTodo } from './routes';
+import { handleError } from './middlewares/handleError';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.get('/todos', getTodos);
 app.post('/todos', postTodo);
 app.delete('/todos/:id', deleteTodo);
+app.use(handleError);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
